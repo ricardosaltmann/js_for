43cd2e8ceb0ca4941fc86dad309bbed270048670
Ricardo <ricardoaltmann@outlook.com.br>
2022-01-18 18:56:51 -0300

    for

---
+++ for.js
@@ -0,0 +1,12 @@
+const numeros = [100, 200, 300, 400, 500, 600]
+
+/* let i =0  utilizamos o i para iniciar o for
+onde o i vai armarzenar o contador
+no segundo parametro fazemos o limite do laco 
+onde enquanto o contador i for menor que o tamanho da lista,
+ele vai continuar o laco
+e no final incrementamos o contador*/
+for (let i = 0; i < numeros.length; i++) {
+    console.log(i, numeros[i])
+    /*vamos exibir o i (contador e o valor da lista) */
+}
